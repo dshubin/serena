7fb53159e0eb4ed29d8797b63ca89195b674aed1
daniel <daniel@daniel-OptiPlex-745.(none)>
2014-03-13 14:20:40 -0700

    added bulk delete of nodes

---
+++ chef/bulkDeleteNode.groovy
@@ -0,0 +1,27 @@
+def props = new Properties();
+final def inputPropsFile = args[0];
+final def inputPropsStream = null;
+
+try {
+	inputPropsStream = new FileInputStream(inputPropsFile);
+	props.load(inputPropsStream);
+} catch (IOException e) {
+	throw new RuntimeException(e);
+} finally {
+	inputPropsStream.close();
+}
+
+final def regex = props["regex"]
+
+def sout = new StringBuffer();
+def serr = new StringBuffer();
+
+def deployCommand = "knife node bulk delete ${regex}"
+def proc = deployCommand.execute();
+proc.consumeProcessOutput(sout, serr);
+proc.waitFor()
+
+println "sout: ${sout}"
+println "serr: ${serr}"
+
+System.exit(0);
